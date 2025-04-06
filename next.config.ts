a387e88dc3fc2e06ec74c33c16ded1523805badf
Andrew Hinger <andrewjhinger@gmail.com>
2025-04-05 21:03:56 -0400

    d

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslist: { ignoreDuringBuilds: true, },
+  eslist: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
